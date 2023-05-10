11551190c0646cdd50b1e6e0ffc5f322a03f3e11
RussellMoore <russell.moore@fox-pest.com>
2023-05-09 22:23:59 -0600

    Set up multi file layer testing

---
+++ tests/Unit/Example2Test.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class Example2Test extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
